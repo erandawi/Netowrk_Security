18804191f5cedc6d5188748b7826122dda53ec02
erandawi <erandalakshan3567@gmail.com>
2024-09-11 22:29:22 +0530

    data validation structure is ready

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -20,73 +20,33 @@ print(MONGO_DB_URL)
 
 
 class DataIngestion:
-    
-    def __init__(self, data_ingestion_config: DataIngestionConfig):
+    def __init__(self):
         try:
-            self.data_ingestion_config = data_ingestion_config
+            pass
         except Exception as e:
-            raise NetworkSecurityException (e, sys)
+            raise NetworkSecurityException(e,sys)
         
     def export_collection_as_dataframe(self):
         try:
-            database_name = self.data_ingestion_config.database_name
-            database_name = self.data_ingestion_config.collection_game
-            
-            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
-            collection = self.mongo_client[database_name][collection_name]
-            
-            df = pd.DataFrame(list(collection.find()))
-            if "_id" in df.columns.to_list():
-                df = df.drop(columns = ["_id"], axis = 1)
-                
-            df.replace({"na": np.nan}, inplace = True)
-            
-            return df
-                
+            pass
         except Exception as e:
-            raise NetworkSecurityException (e, sys)
-    
-    def export_data_into_feature_store(self, dataframe: pd.DataFrame):
+            raise NetworkSecurityException(e,sys)
+        
+    def export_data_into_feature_store(self):
         try:
-            feature_store_file_path = self.data_ingestion_config.feature_store_file_path
-            # creating folder
-            dir_path = os.path.dirname(feature_store_file_path)
-            os.makedirs(dir_path, exist_ok = True)
-            dataframe.to_csv (feature_store_file_path, index = Fasle, header = True)
-            return dataframe
-        
+            pass
         except Exception as e:
-            raise NetworkSecurityException (e, sys)
+            raise NetworkSecurityException(e,sys)
         
-    def split_data_as_train_test(self, dataframe : pd.DataFrame):
+    def split_data_as_train_set(self):
         try:
-            train_set, test_set = train_test_split(
-                dataframe, test_size = self.data_ingestion_config.train_test_split_ratio
-            )
-            
-            dir_path = os.path.dirname(self.data_ingestion_config.traning_file_path)
-            os.makedirs(dir_path, exist_ok = True)
-            logging.info(f"Exporting train and test file path.")
-            train_set.to_csv(
-                self.data_ingestion_config.traning_file_path, index = False, header = True
-            )
-            
-            test_set.to_csv(
-                self.data_ingestion_config.testing_file_path, index = False, header = True
-            )
+            pass
         except Exception as e:
-            raise NetworkSecurityException (e, sys)
+            raise NetworkSecurityException(e,sys)
         
-    def intiate_data_ingestion(self):
+    def intitate_data_ingestion(self):
         try:
-            dataframe = self.export_collection_as_dataframe()
-            dataframe = self.export_data_into_feature_store(dataframe)
-            self.split_data_as_train_test(dataframe = dataframe)
-            
-            data_ingestion_artifact = DataIngestionArtifact(trained_file_path = self.data_ingestion_config.train_file_path,test_file_path = self.data_ingestion_config.testing_file_path)
-            return data_ingestion_artifact
-        
+            pass
         except Exception as e:
-            raise NetworkSecurityException (e, sys)
-        
+            raise NetworkSecurityException(e,sys)
         
--- networksecurity/pipeline/traning_pipeline.py
+++ networksecurity/pipeline/traning_pipeline.py
@@ -1,84 +1,7 @@
-# import os
-# import sys
-
-# from networksecurity.expection.exception import NetworkSecurityException
-# from networksecurity.logger.logger import logging
-
-# from networksecurity.components.data_ingestion import DataIngestion
-# from networksecurity.components.data_validation import DataValidation
-# from networksecurity.components.data_transformation import DataTransformation
-# from networksecurity.components.model_trainer import ModelTrainer
-# from networksecurity.components.model_evaluation import ModelEvaluation
-# from networksecurity.components.model_pusher import ModelPusher
-
-# from networksecurity.entity.config_entity import (
-#     DataIngestionConfig,
-#     DataValidationConfig,
-#     DataTransformationConfig,
-#     ModelTrainerConfig,
-#     ModelEvaluationConfig,
-#     MoodelPusherConfig,
-# )
-
-# from networksecurity.entity.artifact_entity import (
-#     DataIngestionArtifact,
-#     DataValidationArtifact,
-#     DataTransformationArtifact,
-#     ModelTrainerArtifact,
-#     ModelEvaluationArtifact,
-#     MoodelPusherArtifact,
-# )
-
-
-# class TraningPipeline:
-#     def __init__(self):
-#         pass
-
-#     def start_data_ingestion(self):
-#         try:
-#             pass
-#         except Exception as e:
-#             raise NetworkSecurityException(e, sys)
-
-#     def start_data_validation(self):
-#         try:
-#             pass
-#         except Exception as e:
-#             raise NetworkSecurityException(e, sys)
-
-#     def start_data_transformation(self):
-#         try:
-#             pass
-#         except Exception as e:
-#             raise NetworkSecurityException(e, sys)
-
-#     def start_model_trainer(self):
-#         try:
-#             pass
-#         except Exception as e:
-#             raise NetworkSecurityException(e, sys)
-
-#     def start_model_evaluation(self):
-#         try:
-#             pass
-#         except Exception as e:
-#             raise NetworkSecurityException(e, sys)
-
-#     def start_model_pusher(self):
-#         try:
-#             pass
-#         except Exception as e:
-#             raise NetworkSecurityException(e, sys)
-
-#     def run_pipeline(self):
-#         try:
-#             pass
-#         except Exception as e:
-#             raise NetworkSecurityException(e, sys)
 import os
 import sys
 
-from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.expection.exception import NetworkSecurityException
 from networksecurity.logger.logger import logging
 
 from networksecurity.components.data_ingestion import DataIngestion
@@ -88,15 +11,13 @@ from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.components.model_evaluation import ModelEvaluation
 from networksecurity.components.model_pusher import ModelPusher
 
-from networksecurity.entity.config_entity import(
-    TrainingPipelineConfig,
+from networksecurity.entity.config_entity import (
     DataIngestionConfig,
     DataValidationConfig,
     DataTransformationConfig,
     ModelTrainerConfig,
     ModelEvaluationConfig,
-    ModelPusherConfig
-   
+    MoodelPusherConfig,
 )
 
 from networksecurity.entity.artifact_entity import (
@@ -105,62 +26,52 @@ from networksecurity.entity.artifact_entity import (
     DataTransformationArtifact,
     ModelTrainerArtifact,
     ModelEvaluationArtifact,
-    ModelPusherArtifact
+    MoodelPusherArtifact,
 )
 
 
-class TrainingPipeline:
+class TraningPipeline:
     def __init__(self):
-         self.training_pipeline_config = TrainingPipelineConfig()
-    
+        pass
+
     def start_data_ingestion(self):
         try:
-            self.data_ingestion_config=DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
-            logging.info("Starting data ingestion")
-            data_ingestion=DataIngestion(data_ingestion_config=self.data_ingestion_config)
-            data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
-            logging.info(f"Data ingestion completed and artifact: {data_ingestion_artifact}")
-            return data_ingestion_artifact
-            
-            
+            pass
         except Exception as e:
-            raise NetworkSecurityException(e,sys)
-        
+            raise NetworkSecurityException(e, sys)
+
     def start_data_validation(self):
         try:
             pass
         except Exception as e:
-            raise NetworkSecurityException(e,sys)
-        
+            raise NetworkSecurityException(e, sys)
+
     def start_data_transformation(self):
         try:
             pass
         except Exception as e:
-            raise NetworkSecurityException(e,sys)
-    
+            raise NetworkSecurityException(e, sys)
+
     def start_model_trainer(self):
         try:
             pass
         except Exception as e:
-            raise NetworkSecurityException(e,sys)
-        
+            raise NetworkSecurityException(e, sys)
+
     def start_model_evaluation(self):
         try:
             pass
         except Exception as e:
-            raise NetworkSecurityException(e,sys)
-        
+            raise NetworkSecurityException(e, sys)
+
     def start_model_pusher(self):
         try:
             pass
         except Exception as e:
-            raise NetworkSecurityException(e,sys)
-        
+            raise NetworkSecurityException(e, sys)
+
     def run_pipeline(self):
         try:
-            data_ingestion_artifact=self.start_data_ingestion()
-            print(data_ingestion_artifact)
+            pass
         except Exception as e:
-            raise NetworkSecurityException(e,sys)
-        
-    
+            raise NetworkSecurityException(e, sys)
