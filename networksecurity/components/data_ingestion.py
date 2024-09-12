b1a843cf72ac4c38b71296515e0965a11816cbb7
erandawi <erandalakshan3567@gmail.com>
2024-09-12 05:50:35 +0530

    data_ingestion code has written

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -14,39 +14,90 @@ from networksecurity.entity.config_entity import DataIngestionConfig
 from networksecurity.entity.artifact_entity import DataIngestionArtifact
 
 from dotenv import load_dotenv
+
 load_dotenv()
 MONGO_DB_URL = os.getenv("MONGO_DB_URL")
 print(MONGO_DB_URL)
 
 
 class DataIngestion:
-    def __init__(self):
+    def __init__(self, data_ingestion_config: DataIngestionConfig):
         try:
-            pass
+            self.data_ingestion_config = data_ingestion_config
         except Exception as e:
-            raise NetworkSecurityException(e,sys)
-        
+            raise NetworkSecurityException(e, sys)
+
     def export_collection_as_dataframe(self):
         try:
-            pass
+            database_name = self.data_ingestion_config.database_name
+            collection_name = self.data_ingetion_config_collection_name
+
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+            collection = self.mongo_client[database_name][collection_name]
+
+            df = pd.DataFrame(list(collection.find()))
+            if "_id" in df.columns.to_list():
+                df = df.drop(columns=["_id"], axis=1)
+
+            df.replace({"na": np.nan}, inplace=True)
+
+            return df
+
         except Exception as e:
-            raise NetworkSecurityException(e,sys)
-        
-    def export_data_into_feature_store(self):
+            raise NetworkSecurityException(e, sys)
+
+    def export_data_into_feature_store(self, dataframe: pd.DataFrame):
         try:
-            pass
+            feature_store_file_path = self.data_ingestion_config.feature_store_file_path
+            # creating folder
+            dir_path = os.path.dirname(feature_store_file_path)
+            os.makedirs(dir_path, exist_ok=True)
+            dataframe.to_csv(feature_store_file_path, index=False, heder=True)
+            return dataframe
+
         except Exception as e:
-            raise NetworkSecurityException(e,sys)
-        
-    def split_data_as_train_set(self):
+            raise NetworkSecurityException(e, sys)
+
+    def split_data_as_train_set(self, dataframe: pd.DateFrame):
         try:
-            pass
+            train_set, test_set = train_test_split(
+                dataframe, test_size=self.data_ingestion_config.train_test_split_ratio
+            )
+
+            logging.info("Performed train test split on the dataframe")
+
+            logging.info(
+                "Exited split_data_as_train_set method of Data_ingestion class"
+            )
+
+            dir_path = os.path.dirname(self.data_ingestion_config.traning_file_path)
+
+            os.makedirs(dir_path, exit_ok=True)
+
+            logging.info(f"Exporting train and test file path.")
+
+            train_set.to_csv(
+                self.data_ingestion_config.traning_file_path, indeox=False, header=True
+            )
+
+            test_set.to_csv(
+                self.data_ingestion_config.testing_file_path, indeox=False, header=True
+            )
+
         except Exception as e:
-            raise NetworkSecurityException(e,sys)
-        
+            raise NetworkSecurityException(e, sys)
+
     def intitate_data_ingestion(self):
         try:
-            pass
+            dataframe = self.export_collection_as_dataframe()
+            dataframe = self.export_data_into_feature_store(dataframe)
+            self.split_data_as_train_test(dataframe=dataframe)
+
+            data_ingestion_artifact = DataIngestionArtifact(
+                trained_file_path=self.data_ingestion_config.traning_file_path,
+                test_file_path=self.data_ingestion_config.testing_file_path,
+            )
+            return data_ingestion_artifact
+
         except Exception as e:
-            raise NetworkSecurityException(e,sys)
-        
+            raise NetworkSecurityException(e, sys)
