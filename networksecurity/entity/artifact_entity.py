129da759b2fb716e7b8261d672eb7f54fb46f0f7
erandawi <erandalakshan3567@gmail.com>
2024-09-16 23:31:15 +0530

    model_evaluation_added

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -40,7 +40,12 @@ class ModelTrainerArtifact:
 
 @dataclass
 class ModelEvaluationArtifact:
-    pass
+    is_model_accepted: bool
+    improved_accuracy: float
+    best_model_path: str
+    trained_model_path: str
+    train_model_metric_artifact: ClassificationMetricArtifact
+    best_model_metric_artifact: ClassificationMetricArtifact
 
 
 @dataclass
